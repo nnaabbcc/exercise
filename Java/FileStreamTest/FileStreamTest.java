e56e949eba15796486877891371440d2664216d9
nnaabbcc <nnaabbcc.zhao@gmail.com>
2016-11-01 20:43:52 +0800

    FileStreamTest & Dir

---
+++ Java/FileStreamTest/FileStreamTest.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class FileStreamTest
+{
+  public static void main(String args[])
+  {
+    try
+    {
+      // write to file
+      byte[] bWrite = {11, 21, 3, 40, 5};
+      OutputStream os = new FileOutputStream("test.txt");
+      for (int x = 0; x < bWrite.length; x++)
+      {
+        os.write(bWrite[x]); // write the byte
+      }
+      os.close();
+
+      // read from file
+      InputStream is = new FileInputStream("test.txt");
+      int size = is.available();
+
+      for (int i = 0; i < size; i++)
+      {
+        System.out.print((char)is.read() + " ");
+      }
+      is.close();
+    }
+    catch (IOException e)
+    {
+      System.out.print("IO Exception");
+    }
+  }
+}
