6771dc6575d9e50191f646f76dfee7816f56384f
nnaabbcc <nnaabbcc.zhao@gmail.com>
2017-05-30 18:59:41 +0800

    PyQt5 signals and slots example

---
+++ python/PyQt5/signal_slot_test.py
@@ -0,0 +1,28 @@
+
+from PyQt5.QtWidgets import (QApplication, QVBoxLayout, QDialog,
+        QPushButton)
+
+class MyDialog(QDialog):
+
+    def click_slot(self):
+        print('click slot called')
+
+    def __init__(self):
+        super(MyDialog, self).__init__()
+
+        button = QPushButton('Click')
+        button.clicked.connect(self.click_slot)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(button)
+
+        self.setLayout(main_layout)
+        self.setWindowTitle('PyQt5 signals & slots example -- pythonspot.com')
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    myDialog = MyDialog()
+    sys.exit(myDialog.exec_())
+        
