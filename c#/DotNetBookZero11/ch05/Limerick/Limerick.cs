035d22deaf4d9c479fe50bc78c693bf7a6480650
nnaabbcc <nnaabbcc.zhao@gmail.com>
2017-07-02 11:39:55 +0800

    Limerick

---
+++ c#/DotNetBookZero11/ch05/Limerick/Limerick.cs
@@ -0,0 +1,16 @@
+
+using System;
+
+class Limerick
+{
+    static void Main()
+    {
+        string strLimerick = 
+            "There once was a coder named Otto\r\n" +
+            "Who had a peculiar motto:\r\n" +
+            "    \"The goto is king,\r\n" +
+            "    To thee I sing!\"\r\n" +
+            "Maybe that's why he's offen quite blotto\r\n";
+        Console.WriteLine(strLimerick);
+    }
+}
