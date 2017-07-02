bef8adac2a2e958af378d7a7a8423e8e34bd66e7
nnaabbcc <nnaabbcc.zhao@gmail.com>
2017-07-02 11:21:27 +0800

    GetTheUsersName

---
+++ c#/DotNetBookZero11/ch05/GetTheUsersName/GetTheUsersName.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+class GetTheUsersName
+{
+    static void Main()
+    {
+        Console.Write("Type your name and press Enter: ");
+        string strName = Console.ReadLine();
+        Console.Write("Your name is ");
+        Console.WriteLine(strName);
+    }
+}
