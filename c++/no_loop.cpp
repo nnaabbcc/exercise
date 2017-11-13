db2251a913c9c74b1b062f0c92aaf6c42d750d96
nnaabbcc <nnaabbcc.zhao@gmail.com>
2017-11-13 21:01:14 +0800

    define const value for magic number

--- c++/no_loop.cpp
+++ c++/no_loop.cpp
@@ -6,8 +6,12 @@
 
 int main(int, char*[])
 {
-    std::vector<int> v(10);
-    std::generate(v.begin(), v.end(), [](){return std::experimental::randint(0, 1000);});
+    const size_t size = 10;
+    const int range_min = 0;
+    const int range_max = 1000;
+
+    std::vector<int> v(size);
+    std::generate(v.begin(), v.end(), [](){return std::experimental::randint(range_min, range_max);});
 
     std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, "\n"));
 
