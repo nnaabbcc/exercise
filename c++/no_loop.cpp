eaa8c8a100ebde6b49548920273b97f2815e4de9
nnaabbcc <nnaabbcc.zhao@gmail.com>
2017-12-31 20:07:25 +0800

    update no_loop

--- c++/no_loop.cpp
+++ c++/no_loop.cpp
@@ -3,6 +3,7 @@
 #include <experimental/random>
 #include <iostream>
 #include <iterator>
+#include <functional>
 
 int main(int, char*[])
 {
@@ -11,9 +12,18 @@ int main(int, char*[])
     const int range_max = 1000;
 
     std::vector<int> v(size);
+    // lamda function
     std::generate(v.begin(), v.end(), [](){return std::experimental::randint(range_min, range_max);});
+    // bind 
+    std::generate(v.begin(), v.end(), std::bind(std::experimental::randint<int>, range_min, range_max));
+
+    std::srand(time(0));
+    std::generate(v.begin(), v.end(), [](){
+        return static_cast<int>(range_min + std::rand() * 1.0 / RAND_MAX * (range_max - range_min));
+        });
 
     std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, "\n"));
+
 
     return 0;
 }
