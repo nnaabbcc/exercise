a8ed8eab0967b0b469ff9ad3c4290fd53b83e462
yulang.zhao <yulang.zhao@localhost>
2020-03-03 09:56:41 +0800

    Subplot

---
+++ python/plot/exercise_subplot_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#!encoding=utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f(t):
+    return np.exp(-t) * np.cos(2 * np.pi * t)
+
+if __name__ == '__main__' :
+    t1 = np.arange(0, 5, 0.1)
+    t2 = np.arange(0, 5, 0.02)
+
+    plt.figure(1)
+    plt.subplot(2, 2, 1)
+    plt.plot(t1, f(t1), 'bo', t2, f(t2), 'r--')
+
+    plt.subplot(2, 2, 2)
+    plt.plot(t2, np.cos(2 * np.pi * t2), 'r--')
+
+    plt.subplot(2, 1, 2)
+    plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+
+    plt.show()
